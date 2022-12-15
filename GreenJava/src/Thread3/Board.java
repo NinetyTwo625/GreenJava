701f09edf7da3233402ba1bcda7a4558938844ca
Administrator <Administrator@User-2022ZEVISB>
2022-12-15 16:58:39 +0900

    221215 진도4

---
+++ GreenJava/src/Thread3/Board.java
@@ -0,0 +1,12 @@
+package Thread3;
+
+public class Board {
+	String subject;
+	String content;
+
+	public Board(String subject, String content) {
+		this.subject = subject;
+		this.content = content;
+	}
+
+}
